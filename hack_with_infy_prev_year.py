8a91895ce2e56c857f26c2223e256a9882e33d76
GARIMA-JEENA <69374495+GARIMA-JEENA@users.noreply.github.com>
2021-04-27 20:46:04 +0530

    python file added

---
+++ hack_with_infy_prev_year.py
@@ -0,0 +1,32 @@
+# n,m=map(int,input().split())
+# a=list(map(int,input().strip().split()))[:n]
+# b=list(map(int,input().strip().split()))[:m]
+# count=0
+# for i in range(1,101):
+#     flag=1
+#     for j in a:
+#         if(j!=0):
+#             if (i%j)!=0:
+#                 flag=0
+#                 break
+#     if(flag==1):
+#         for k in b:
+#             if (k%i)!=0:
+#                 flag=0
+#                 break
+#     if(flag==1):
+#         count+=1 
+#         print("seeeeeee : ",i)   
+# print(count)
+
+
+
+
+
+
+
+
+
+
+
+    
